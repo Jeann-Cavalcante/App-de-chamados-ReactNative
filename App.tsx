654708e9ece3372f55e324fcc977b4a33bd0fb75
Jean <jeanncavalcante@gmail.com>
2022-07-18 22:20:18 -0300

    libs

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import { NativeBaseProvider } from "native-base";
+import { NativeBaseProvider, StatusBar } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { SingIn } from "./src/screens/SingnIn";
+import { THEME } from "./src/styles/theme";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
-    <NativeBaseProvider>
-      <SingIn />
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <SingIn /> : <Loading />}
     </NativeBaseProvider>
   );
 }
