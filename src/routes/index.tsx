9c395a3860c318a55b2e3c2c333164e7f79c7e92
Jean <jeanncavalcante@gmail.com>
2022-07-21 23:19:17 -0300

    auth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,13 @@
+import auth, { FirebaseAuthTypes } from "@react-native-firebase/auth";
 import { NavigationContainer } from "@react-navigation/native";
+import { useState } from "react";
 
 import { SingIn } from "../screens/SingnIn";
 import { AppRoutes } from "./app.routes";
 
 export function Routes() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useState<FirebaseAuthTypes.User>(null);
   return (
     <NavigationContainer>
       <SingIn />
--- src/screens/SingnIn.tsx
+++ src/screens/SingnIn.tsx
@@ -1,4 +1,4 @@
-// import auth from "@react-native-firebase/auth";
+import auth from "@react-native-firebase/auth";
 import { Heading, Icon, useTheme, VStack } from "native-base";
 import { Envelope, Key } from "phosphor-react-native";
 import { useState } from "react";
@@ -19,6 +19,30 @@ export function SingIn() {
       return;
     }
     setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log("Login realizado com sucesso");
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+
+        if (error.code === "auth/invalid-email") {
+          return Alert.alert("Error", "Email inválido.");
+        }
+
+        if (error.code === "auth/wrong-password") {
+          return Alert.alert("Error", "Email ou senha inválido.");
+        }
+
+        if (error.code === "auth/user-not-found") {
+          return Alert.alert("Error", "Email ou senha inválido.");
+        }
+
+        return Alert.alert("Error", "Erro ao fazer login.");
+      });
   }
 
   const { colors } = useTheme(); //hook para usar thema
